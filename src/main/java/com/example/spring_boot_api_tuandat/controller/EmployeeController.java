6118f51b7c2ccf7c7738edef7c1f936d731b823b
dadasaf111 <vanteonguyen955@gmail.com>
2021-09-15 17:34:33 +0700

    Commit CRUD Basic

--- src/main/java/com/example/spring_boot_api_tuandat/controller/EmployeeController.java
+++ src/main/java/com/example/spring_boot_api_tuandat/controller/EmployeeController.java
@@ -1,11 +1,36 @@
 package com.example.spring_boot_api_tuandat.controller;
 
-import com.example.spring_boot_api_tuandat.service.EmployeeServiceImpl;
+import com.example.spring_boot_api_tuandat.entity.Employee;
+import com.example.spring_boot_api_tuandat.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
     @Autowired
-    private EmployeeServiceImpl employeeService;
+    private EmployeeService employeeService;
+
+    @PostMapping("/employees")
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable("id") Long employeeId){
+        return employeeService.getEmployeeById(employeeId);
+    }
+    @PutMapping("/employees/{id}")
+    public Employee updateEmployeeById(@PathVariable("id") Long employeeId,@RequestBody Employee employee){
+        return  employeeService.updateEmployeeById(employeeId,employee);
+    }
+    @DeleteMapping("/employees/{id}")
+    public String deleteById(@PathVariable("id") Long employeeId){
+        employeeService.deleteById(employeeId);
+        return "Delete Sucessfully!";
+    }
 }
--- src/main/java/com/example/spring_boot_api_tuandat/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/spring_boot_api_tuandat/service/EmployeeServiceImpl.java
@@ -1,35 +1,54 @@
 package com.example.spring_boot_api_tuandat.service;
 
 import com.example.spring_boot_api_tuandat.entity.Employee;
+import com.example.spring_boot_api_tuandat.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Override
     public Employee saveEmployee(Employee employee) {
-        return null;
+        return employeeRepository.save(employee);
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        return (List<Employee>) employeeRepository.findAll();
     }
 
     @Override
     public Employee getEmployeeById(Long id) {
-        return null;
+        return employeeRepository.findById(id).get();
     }
 
     @Override
     public void deleteById(Long id) {
-
+        employeeRepository.deleteById(id);
     }
 
     @Override
     public Employee updateEmployeeById(Long id, Employee employee) {
-        return null;
+        Employee employeeDB= employeeRepository.findById(id).get();
+
+        if(Objects.nonNull(employee.getFirstName()) && !"".equalsIgnoreCase(employee.getFirstName())){
+            employeeDB.setFirstName(employee.getFirstName());
+        }
+
+        if(Objects.nonNull(employee.getLastName()) && !"".equalsIgnoreCase(employee.getLastName())){
+            employeeDB.setLastName(employee.getLastName());
+        }
+
+        if(Objects.nonNull(employee.getEmail()) && !"".equalsIgnoreCase(employee.getEmail())){
+            employeeDB.setEmail(employee.getEmail());
+        }
+        return employeeRepository.save(employeeDB);
     }
 }
